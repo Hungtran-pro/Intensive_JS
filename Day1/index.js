be9b12e5f6cec3e97d78086ce0e6d7f6e979f495
Tran Hung <Hungtran-pro@users.noreply.github.com>
2020-11-18 16:41:17 +0700

    Initial commit

---
+++ Day1/index.js
@@ -0,0 +1,30 @@
+// let inputYear = prompt('Enter a year you want');
+// let centuryFromYear = Math.ceil(inputYear/100);
+// console.log(centuryFromYear);
+
+// let stringInput = prompt('Enter a string you want');
+// let check = true;
+// let len = stringInput.length;
+// for(let i = 0; i < len / 2; i++){
+//     if(stringInput[i] != stringInput[len - i - 1]) check = false;
+// }
+// if(check) console.log('This is a palindrome');
+// else console.log('This is not palindrome');
+
+let n = prompt("Enter n number in an array you want");
+let array = [];
+for (let i = 0; i < n; i++) {
+  let tmp = prompt("Enter a number");
+  array.push(tmp);
+}
+// let sum = array[0] + array[1];
+// for(let i = 1; i < n - 1; i++){
+//     if(array[i] + array[i+1] > sum) sum = array[i] * array[i+1];
+// }
+// console.log(array);
+// console.log(sum);
+
+let oddNumbers = array.filter(function (item) {
+  return item % 2 == 1;
+});
+console.log(oddNumbers);
