53e69df7018b921797168a54f5044f9290d419bd
Tran Hung <Hungtran-pro@users.noreply.github.com>
2020-11-25 21:52:23 +0700

    baitapday2

---
+++ Day2-hw/index.js
@@ -0,0 +1,96 @@
+class Students {
+  classname;
+  fname;
+  lname;
+  age;
+  hometown;
+  constructor(classname, fname, lname, age, hometown) {
+    this.classname = classname;
+    this.fname = fname;
+    this.lname = lname;
+    this.age = age;
+    this.hometown = hometown;
+  }
+  update(classname, fname, lname, age, hometown) {
+    this.classname = classname;
+    this.fname = fname;
+    this.lname = lname;
+    this.age = age;
+    this.hometown = hometown;
+  }
+  //   toLowerCase(){
+  //       return this.hometown.toLowerCase();
+  //   }
+  toHTML() {
+    return `<div class = "container">
+            <div class="inforStudent">
+                <div>${this.classname}</div>
+                <div>${this.fname}</div>
+                <div>${this.lname}</div>
+                <div>${this.age}</div>
+                <div>${this.hometown}</div>
+            </div>
+        </div>
+        `;
+  }
+}
+
+class ListStudents {
+  listStudents;
+  constructor() {
+    this.listStudents = [];
+  }
+  addStudent(student) {
+    this.listStudents.push(student);
+  }
+  removeStudent(index) {
+    this.listStudents.slice(index, 1);
+  }
+  showIndexOfStudent(index) {
+    document.querySelector(".list").innerHTML = this.listStudents[
+      index
+    ].toHTML();
+  }
+  showStudent() {}
+  findAge(age) {
+    for (let i = 0; i < this.listStudents.length; i++) {
+      if (this.listStudents[i].age === age) {
+        this.showStudent(i);
+      }
+    }
+  }
+  findAge18() {
+    let count = 0;
+    let countAge = 0;
+    let countHomeTown = 0;
+    for (let i = 0; i < this.listStudents.length; i++) {
+      let check = 0;
+      if (this.listStudents[i].age === 18) {
+        countAge++;
+        check++;
+      }
+      if (this.listStudents[i].hometown.toLowerCase() === "hanoi") {
+        countHomeTown++;
+        check++;
+      }
+      if (check == 2) count++;
+    }
+    console.log(`Số học sinh 18 tuổi là ${countAge}`);
+    console.log(`Số học sinh quê ở Hà Nội là ${countHomeTown}`);
+    console.log(`Số học sinh 18 tuổi và quê ở Hà Nội là ${count}`);
+  }
+  findStudentsByName(fname, lname){
+    for(let i = 0; i < this.listStudents.length; i++){
+        if(this.listStudents[i].fname === fname && this.listStudents[i].lname === lname){
+            this.showIndexOfStudent(i);
+        }
+    }
+  }
+}
+
+let listStudents = new ListStudents();
+const student1 = new Students("12A2", "hung", "tran", 15, "HaNoi");
+listStudents.addStudent(student1);
+listStudents.showStudent();
+listStudents.findAge18();
+listStudents.findStudentsByName('hung', 'tran');
--- test/index.js
+++ test/index.js
@@ -1,19 +1,13 @@
-let noOver = document.getElementById('no');
-let yesOver = document.getElementById('yes');
-let answerBox = document.getElementsByClassName('answer');
-
-noOver.addEventListener('mouseover', reverseAns);
-yesOver.addEventListener('click', yesss);
-
-function reverseAns (){
-    if(answerBox[0].style.flexDirection === 'row-reverse'){
-        answerBox[0].style.flexDirection = 'row';
-    }
-    else {
-        answerBox[0].style.flexDirection = 'row-reverse';
+function DogObject(name, age) {
+    let dog = Object.create(constructorObject);
+    dog.name = name;
+    dog.age = age;
+    return dog;
+}
+let constructorObject = {
+    speak: function(){
+        return "I am a dog"
     }
 }
-
-function yesss (){
-    alert('I love u');
-}
+let bingo = DogObject("Bingo", 54);
+console.log(bingo);
