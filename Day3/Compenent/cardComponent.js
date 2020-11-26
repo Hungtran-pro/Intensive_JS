1c3d98d99347d5b678c3aecfe017c91af8863de5
Tran Hung <Hungtran-pro@users.noreply.github.com>
2020-11-26 22:16:19 +0700

    Day3

--- Day3/Compenent/cardComponent.js
+++ Day3/Compenent/cardComponent.js
@@ -1,11 +1,44 @@
+const style = `
+    <style>
+    .card{
+        font-family: sans-serif;
+        border: #323b34 solid 1px;
+        margin-right: 20px;
+    }
+    .title{
+        font-size: 1.5rem;
+        font-weight: 600;
+    }
+    .card-body{
+        border: #323b34 solid 1px;
+        padding: 20px;
+    }
+    .card > img {
+        width: 20vw;
+        height: 20vw;
+    }
+    .title, .description {
+        text-align: center;
+    }
+    </style>
+`
+
 class CardComponent extends HTMLElement {
   constructor() {
     super();
     //khai bao shadow_dom
     this._shadowDom = this.attachShadow({ mode: "open" });
+    this.imgSrc = this.getAttribute('imgSrc');
+    this.tle = this.getAttribute('tle');
+    this.description = this.getAttribute('dcrp');
     this._shadowDom.innerHTML = `
-        <div>
-            <h1>This is my component</h1>
+        ${style}
+        <div class="card">
+            <img src="${this.imgSrc}">
+            <div class='card-body'>
+                <div class="title">${this.tle} </div>
+                <div class="description">${this.description} </div>
+            </div>
         </div>
         `;
   }
